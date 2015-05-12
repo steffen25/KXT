1540df60ebce06f74af30e982c8d886a77fdd012
steffen25 <acbolic@gmail.com>
2015-05-12 10:14:09 +0200

    Database klasse

--- src/kxt/DBConnector.java
+++ src/kxt/DBConnector.java
@@ -1,7 +1,77 @@
 package kxt;
 
-/**
- * Created by danniwu on 11/05/15.
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-public class DBConnector {
+
+/**
+ *
+ * @author Steffen
+ */
+import com.mysql.jdbc.Connection;
+import java.sql.*;
+import java.sql.DriverManager;
+/**
+ * @desc A singleton database access class for MySQL
+ * @author Ramindu
+ */
+public final class DBConnector {
+    public Connection conn;
+    private Statement statement;
+    public static DBConnector db;
+    DBConnector() {
+        String url= "jdbc:mysql://localhost:3306/";
+        String dbName = "todo2";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "root";
+        String password = "";
+        try {
+            Class.forName(driver).newInstance();
+            this.conn = (Connection)DriverManager.getConnection(url+dbName,userName,password);
+            System.out.println("Connected to DB");
+        }
+        catch (Exception sqle) {
+            sqle.printStackTrace();
+        }
+    }
+    /**
+     *
+     * @return MysqlConnect Database connection object
+     */
+    public static synchronized DBConnector getDbCon() {
+        if ( db == null ) {
+            db = new DBConnector();
+        }
+        return db;
+ 
+    }
+    
+    
+    /**
+     *
+     * @param query String The query to be executed
+     * @return a ResultSet object containing the results or null if not available
+     * @throws SQLException
+     */
+    public ResultSet query(String query) throws SQLException{
+        statement = db.conn.createStatement();
+        ResultSet res = statement.executeQuery(query);
+        return res;
+    }
+    /**
+     * @desc Method to insert data to a table
+     * @param insertQuery String The Insert query
+     * @return boolean
+     * @throws SQLException
+     */
+    public int insert(String insertQuery) throws SQLException {
+        statement = db.conn.createStatement();
+        int result = statement.executeUpdate(insertQuery);
+        return result;
+ 
+    }
+ 
 }
+
