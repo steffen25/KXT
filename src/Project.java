da811221a1e881e08e9eee538f6090d0bd214238
Wumaniizer <danniwu93@gmail.com>
2015-05-12 13:15:40 +0200

    Project class - har added attributter

--- src/Project.java
+++ src/Project.java
@@ -1,7 +1,43 @@
-
+import java.util.ArrayList;
 
 /**
  * Created by danniwu on 11/05/15.
  */
 public class Project {
+    public String name;
+    public int status;
+    public ArrayList phaes;
+    public ArrayList tasks;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public ArrayList getPhaes() {
+        return phaes;
+    }
+
+    public void setPhaes(ArrayList phaes) {
+        this.phaes = phaes;
+    }
+
+    public ArrayList getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList tasks) {
+        this.tasks = tasks;
+    }
 }
