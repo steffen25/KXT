ed19fcc0e8c3d47deadabb43b75d73985b540cca
steffen25 <acbolic@gmail.com>
2015-05-14 18:05:58 +0200

    LOGIN fixet

--- src/LoginController.java
+++ src/LoginController.java
@@ -15,71 +15,33 @@ import org.json.JSONObject;
 public class LoginController implements ActionListener {
     
     BasicPasswordEncryptor passwordEncryptor = new BasicPasswordEncryptor();
+
+    private LoginView view;
     
-  /** Action command string. */
-  static final String LOGIN = "Login";
-  
-  /** Action command string. */
-  static final String CANCEL = "Cancel";
-  
-  
-  
-  //  PRIVATE 
-  private LoginUI loginView = new LoginUI(this);
-  private int loginNumAttempts = 0;
-  private static final int MAX_NUM_ATTEMPTS = 3;
-  
+    private LoginModel model;
+
+    public LoginController(LoginView view, LoginModel model) {
+        this.view = view;
+        this.model = model;
+        System.out.println("Database connection: " + Database.isConnected());
+        view.getLoginButton().addActionListener(this);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-    String command = e.getActionCommand();
-    if ( LOGIN.equals(command) ) {
-        System.out.println("klikket på login");
-        System.out.println(loginView.getUserName());
-        System.out.println("test" +loginView.getUserName());
-        
-      validateUserCredentials();
+        if(e.getActionCommand().equals("Login"))
+        {
+            System.out.println(view.getUsername() + " " +view.getPassword());
+            Database.query("INSERT INTO projektstyring_test (username, password, email) VALUES ('"+view.getUsername()+"','"+view.getPassword()+"','email@email.com')");
+            //System.out.println(result);
+            
+        }
+        //if(view.getu)
     }
-    else if( CANCEL.equals(command) ){
-      System.exit(0);
-    }
-    else {
-      throw new AssertionError("Unexpected command: " + command);
-    }
-  }
     
     
     
-    
-    
-    void validateUserCredentials(){
-    loginNumAttempts++;
-    String userName = loginView.getUserName();
-    String password = loginView.getPassword();
-    boolean connected = Database.isConnected();
-    String encryptedPassword = null;
-    if (connected)
-    {
-        JSONObject result = Database.query("SELECT FROM users WHERE = '"+ userName +"'");
-        JSONArray tmp = result.getJSONArray("results");
-        JSONObject obj = tmp.getJSONObject(0);
-        encryptedPassword = obj.getString("pwd");
-    }
-    if (passwordEncryptor.checkPassword(password, encryptedPassword)){
-      loginView.dispose();
-      System.out.println("Correct");
-    }
-    else {
-      if(loginNumAttempts < MAX_NUM_ATTEMPTS) {
-        //fView.tryAgain();
-      }
-      else {
-        //fLogger.config("Shutting down. User credentials not valid for more than the max number of tries.");
-        //shutDownApplication();
-      }
-    }
-  }
-    
+
     
     
     
