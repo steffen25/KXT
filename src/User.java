893661b4cb2096dd8a72bad1875433cc7f7a3439
Wumaniizer <danniwu93@gmail.com>
2015-05-18 12:24:50 +0200

    User class - get & set

--- src/User.java
+++ src/User.java
@@ -4,5 +4,35 @@
  * Created by danniwu on 11/05/15.
  */
 public class User {
+
+    public String name;
+    public String initials;
+    public int level;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getInitials() {
+        return initials;
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
 }
 
